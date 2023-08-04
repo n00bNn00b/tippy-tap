7ef2c62d01c0777a71fe484b2127719cbde432ab
n00bNn00b <dewanreal@gmail.com>
2023-08-04 12:01:29 +0600

    signup component has been created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup/Signup";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   );
 }
---
+++ client/src/components/Signup/Signup.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="card flex mx-auto w-96 bg-base-100 shadow-xl">
+      <figure>
+        <h1 className="font-bold">Sign Up</h1>
+      </figure>
+      <form className="mx-5">
+        <label className="label">
+          <span className="label-text">
+            First Name <span className="text-error">*</span>
+          </span>
+        </label>
+        <input
+          type="text"
+          placeholder="i.e: Felicia"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <label className="label">
+          <span className="label-text">Middle Name</span>
+        </label>
+        <input
+          type="text"
+          placeholder="(Optional)"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <label className="label">
+          <span className="label-text">
+            Last Name <span className="text-error">*</span>
+          </span>
+        </label>
+        <input
+          type="text"
+          placeholder="i.e: Herman"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <label className="label">
+          <span className="label-text">
+            Email <span className="text-error">*</span>
+          </span>
+        </label>
+        <input
+          type="email"
+          placeholder="i.e: felicia@example.com"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <label className="label">
+          <span className="label-text">
+            Phone <span className="text-error">*</span>
+          </span>
+        </label>
+        <input
+          type="text"
+          placeholder="+123456789"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <label className="label">
+          <span className="label-text">
+            Password <span className="text-error">*</span>
+          </span>
+        </label>
+        <input
+          type="password"
+          placeholder="Type here"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <label className="label">
+          <span className="label-text">
+            Confirm Password <span className="text-error">*</span>
+          </span>
+        </label>
+        <input
+          type="password"
+          placeholder="Type here"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          type="submit"
+          value="Sign Up"
+          className="btn btn-primary flex mx-auto my-3"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
