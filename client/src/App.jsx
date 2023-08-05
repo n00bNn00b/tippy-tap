33a642cb0df138f4b0c54ba925f02a219dcbe46f
n00bNn00b <dewanreal@gmail.com>
2023-08-05 06:25:01 +0600

    signup api implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup/Signup";
 import Login from "./components/Login/Login";
 import ResetPassWord from "./components/ResetPassWord/ResetPassWord";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/reset" element={<ResetPassWord />} />
       </Routes>
+      <ToastContainer />
     </>
   );
 }
--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -1,12 +1,73 @@
 import React from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Signup = () => {
+  const url = "http://localhost:5000/signup";
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    const firstName = e.target.firstName.value;
+    const middleName = e.target.middleName.value || " ";
+    const lastName = e.target.lastName.value;
+    const email = e.target.email.value;
+    const phone = e.target.phone.value;
+    const password = e.target.password.value;
+    const confirmPassword = e.target.confirmPassword.value;
+    const userID = Math.floor(Math.random() * 1000 * 10000000);
+
+    console.log(
+      firstName,
+      middleName,
+      lastName,
+      email,
+      phone,
+      password,
+      confirmPassword,
+      userID
+    );
+    if (password === confirmPassword) {
+      await axios
+        .post(
+          url,
+          {
+            userID,
+            firstName,
+            middleName,
+            lastName,
+            email,
+            phone,
+            password,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+          if (res.status === 422 || !res.data) {
+            toast.error("Invalid Registration!");
+          } else {
+            toast.success(res.data.message);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          if (err.response.status === 422) {
+            toast.warning(err.response.data.error);
+          }
+        });
+    } else {
+      toast.error("Password did not match!");
+    }
+  };
   return (
     <div className="card my-20 flex mx-auto w-96 bg-base-100 shadow-2xl">
       <figure>
         <h1 className="font-bold">Sign Up</h1>
       </figure>
-      <form className="card-body">
+      <form onSubmit={handleSignUp} className="card-body">
         <label className="label">
           <span className="label-text">
             First Name <span className="text-error">*</span>
@@ -15,7 +76,9 @@ const Signup = () => {
         <input
           type="text"
           placeholder="i.e: Felicia"
+          name="firstName"
           className="input input-bordered w-full max-w-xs"
+          required
         />
         <label className="label">
           <span className="label-text">Middle Name</span>
@@ -23,6 +86,7 @@ const Signup = () => {
         <input
           type="text"
           placeholder="(Optional)"
+          name="middleName"
           className="input input-bordered w-full max-w-xs"
         />
         <label className="label">
@@ -33,7 +97,9 @@ const Signup = () => {
         <input
           type="text"
           placeholder="i.e: Herman"
+          name="lastName"
           className="input input-bordered w-full max-w-xs"
+          required
         />
         <label className="label">
           <span className="label-text">
@@ -43,7 +109,9 @@ const Signup = () => {
         <input
           type="email"
           placeholder="i.e: felicia@example.com"
+          name="email"
           className="input input-bordered w-full max-w-xs"
+          required
         />
         <label className="label">
           <span className="label-text">
@@ -53,6 +121,7 @@ const Signup = () => {
         <input
           type="text"
           placeholder="+123456789"
+          name="phone"
           className="input input-bordered w-full max-w-xs"
         />
         <label className="label">
@@ -63,7 +132,9 @@ const Signup = () => {
         <input
           type="password"
           placeholder="Type here"
+          name="password"
           className="input input-bordered w-full max-w-xs"
+          required
         />
         <label className="label">
           <span className="label-text">
@@ -73,7 +144,9 @@ const Signup = () => {
         <input
           type="password"
           placeholder="Type here"
+          name="confirmPassword"
           className="input input-bordered w-full max-w-xs"
+          required
         />
         <input
           type="submit"
